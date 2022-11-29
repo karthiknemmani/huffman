decf0bfbeb491af57c695c2c197116dfea6dab2e
karthiknemmani <karthiknemmani@gmail.com>
2022-11-29 01:02:03 -0600

    updated tree

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
+import java.util.HashMap;
 
 public class HuffmanTree {
     public static int[] buildFrequencyArray(InputStream in) throws IOException {
@@ -16,31 +18,47 @@ public class HuffmanTree {
 
     public static TreeNode buildHuffmanTree(int[] frequencies) {
         PriorityQueue314<TreeNode> treeQueue = initialfrequencyQueue(frequencies);
-        // set initial subtree and enqueue weight node
-        TreeNode parentNode = setInitialSubtree(treeQueue);
-        treeQueue.enqueue(parentNode);
-        
-    }
-
-    private static TreeNode setInitialSubtree(PriorityQueue314<TreeNode> treeQueue) {
-        TreeNode leftNode = treeQueue.dequeue();
-        TreeNode rightNode = treeQueue.dequeue();
-        int parentFrequency = leftNode.getFrequency() + rightNode.getFrequency();
-        TreeNode parentNode = new TreeNode(leftNode, parentFrequency, rightNode);
-        return parentNode;
+        // build tree
+        while (treeQueue.size() > 1) { // can build subtrees
+            TreeNode leftNode = treeQueue.dequeue();
+            TreeNode rightNode = treeQueue.dequeue();
+            int parentFrequency = leftNode.getFrequency() + rightNode.getFrequency();
+            TreeNode parentNode = new TreeNode(leftNode, parentFrequency, rightNode);
+            treeQueue.enqueue(parentNode);
+        }
+        TreeNode root = treeQueue.dequeue(); // reached the root node
+        return root;
     }
 
     private static PriorityQueue314<TreeNode> initialfrequencyQueue(int[] frequencies) {
         PriorityQueue314<TreeNode> treeQueue = new PriorityQueue314<>();
-        // enqueue all frequency nodes
+        // enqueue all frequency nodes where node exists
         for (int frequency: frequencies) {
             if (frequency != -1) {
                 TreeNode bitNode = new TreeNode(null, frequency, null);
-            treeQueue.enqueue(bitNode);
+                treeQueue.enqueue(bitNode);
             }
-            }
-            
         }
         return treeQueue;
     }
+
+    public static Map<Integer, String> createMap(TreeNode root) {
+        if (root == null) {
+            throw new IllegalArgumentException("Root cannot be null");
+        }
+        Map<Integer, String> huffMap = new HashMap<>();
+        recurse(huffMap, root, "");
+        return huffMap;
+    }
+
+    private static void recurse(Map<Integer, String> huffMap, TreeNode huffNode, String path) {
+        if (huffNode.isLeaf()) {
+            huffMap.put(huffNode.getValue(), path);
+        }
+        recurse(huffMap, huffNode.getLeft(), path + "0");
+        recurse(huffMap, huffNode.getRight(), path + "1");
+    }
+
+
+
 }
